f10feac5d533515069c1be44c7eb904e58d5c6ca
Rafael-sant1 <rsantannasantos@gmail.com>
2025-09-17 10:58:15 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import { useState } from 'react'
 import './App.css'
-import Bmw from './assets/Bmw.avif'
-import palmeiras from './assets/palmeiras.avif'
-import sapato from './assets/sapato.avif'
-import Logo from './assets/pumabranc.png'
+import suedAlien1 from './assets/sued-alien-1.avif'
+import LogoPuma from './assets/logo-puma-branco.png'
+import LogoInsta from './assets/instagram.svg'
+import LogoDiscord from './assets/discord.svg'
+import LogoWhats from './assets/whatsapp.svg'
+import Slider1 from './assets/1.jpg'
+import Slider2 from './assets/2.jpg'
+import Slider3 from './assets/3.jpg'
+import Slider4 from './assets/4.jpg'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplayz } from 'swiper/modules'
+import 'swiper/css';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,52 +19,104 @@ function App() {
   return (
     <>
       <main>
-        <header></header>
+        <header>
+          {/* Aqui fica o SLIDER */}  
+          <Swiper
+            modules={[Autoplay]}
+            spaceBetween={0}
+            slidesPerView={1}
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: false,
+            }}
+            loop={true}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <img src={Slider1} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={Slider2} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={Slider3} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src={Slider4} alt="" />
+            </SwiperSlide>
+            ...
+          </Swiper>
+        </header>
         <section className="produtos">
+          
           {/* Card 1 */}
           <a href="#" className="link-card">
-          <div className="card">
-            <img src={Bmw} alt="" className="img-card"/>
-            <h2>Tênis RS-X BMW M Motorsport Unissex</h2>
-            <p className="desc"> Apresentando gráficos extraídos do design de ponta do carro e uma pintura renovada, esta coleção presta homenagem à velocidade, precisão e inovação que continuam a definir este ícone.</p>
-            <p className="classificacao"> <span>★ ★ ★ ☆ ☆</span> (224)</p>
-            <p className="preco">R$ 799,99</p>
-            <p className="off">-5%</p>
-          </div>
+            <div className="card">
+              <img src={suedAlien1} alt="" className="img-card"/>
+              <h2>Tênis Suede XL Alien</h2>
+              <p className="desc">Formstrip bordado e uma gola acolchoada exagerada, ele mistura herança e streetwear moderno.</p>
+              <p className="classificacao"> <span>★ ★ ★ ★ ☆</span> (479)</p>
+              <p className="preco">R$ 599,99</p>
+              <p className="off">- 5%</p>
+            </div>
           </a>
-          {/* card2 */}
+
+          {/* Card 2 */}
           <a href="#" className="link-card">
-          <div className="card">
-            <img src={palmeiras} alt="" className="img-card"/>
-            <h2>Moletom com Capuz SE Palmeiras ftblNRGY+ Masculino</h2>
-            <p className="desc"> Comemore seu orgulho pelo Palmeiras com este moletom com capuz PUMA</p>
-            <p className="classificacao"> <span>★ ★ ★ ★ ☆</span>  (504)</p>
-            <p className="preco">R$ 749,99</p>
-            <p className="off">-10%</p>
-          </div>
+            <div className="card">
+              <img src={suedAlien1} alt="" className="img-card"/>
+              <h2>Tênis Suede XL Alien</h2>
+              <p className="desc">Formstrip bordado e uma gola acolchoada exagerada, ele mistura herança e streetwear moderno.</p>
+              <p className="classificacao"> <span>★ ★ ★ ★ ☆</span> (479)</p>
+              <p className="preco">R$ 599,99</p>
+              <p className="off">- 5%</p>
+            </div>
           </a>
-          {/* card3 */}
+
+          {/* Card 3 */}
           <a href="#" className="link-card">
-          <div className="card">
-            <img src={sapato} alt="" className="img-card"/>
-            <h2>Tênis RS-X Suede</h2>
-            <p className="desc"> O RS-X está de volta. Essa silhueta retrô do futuro fez sucesso quando foi lançada em 2018, celebrada por seu design disruptivo</p>
-            <p className="classificacao"> <span>★ ★ ★ ★ ★ </span> (954)</p>
-            <p className="preco">R$ 649,99</p>
-            <p className="off">-10%</p>
-          </div>
+            <div className="card">
+              <img src={suedAlien1} alt="" className="img-card"/>
+              <h2>Tênis Suede XL Alien</h2>
+              <p className="desc">Formstrip bordado e uma gola acolchoada exagerada, ele mistura herança e streetwear moderno.</p>
+              <p className="classificacao"> <span>★ ★ ★ ★ ☆</span> (479)</p>
+              <p className="preco">R$ 599,99</p>
+              <p className="off">- 5%</p>
+            </div>
           </a>
 
         </section>
         <section className="destaque">
 
-          <iframe src="https://www.youtube.com/embed/5hqEX3Kt560?autoplay=1&mute=1&controls=0&loop=1&playlist=5hqEX3Kt560&modestbranding=1&showinfo=0" frameborder="0" allow="autoplay; fullscreen"></iframe>
+          <iframe src="https://www.youtube.com/embed/p7cDN1ofIw0?autoplay=1&mute=1&controls=0&loop=1&playlist=p7cDN1ofIw0&modestbranding=1&showinfo=0" frameborder="0" allow="autoplay; fullscreen"></iframe>
 
         </section>
+
         <footer>
 
-        <img src={Logo} alt="" />
-        <p className="txt-footer">PUMA SPORTS LTDA CNPJ 05.406.034/0023-00 Inscrição Estadual 278.205.224.119 Endereço: Rua Passadena 104 - Parque Industrial, Cotia - SP CEP: 06715864.</p>
+          <img src={LogoPuma} alt="" className="logo-footer" />
+
+          <div className="social-footer">
+            <div className="icon-social">
+              <a href="https://www.instagram.com/pumabrasil/" target="_blank">
+                <img src={LogoInsta} alt="" className="instagram" />
+              </a>
+            </div>
+            <div className="icon-social">
+              <a href="https://discord.com/invite/puma" target="_blank">
+                <img src={LogoDiscord} alt="" className="discord" />
+              </a>
+            </div>
+            <div className="icon-social">
+              <a href="https://web.whatsapp.com/" target="_blank">
+                <img src={LogoWhats} alt="" className="whatsapp" />
+              </a>
+            </div>
+          </div>
+
+          <p className="txt-footer">
+            PUMA SPORTS LTDA CNPJ 05.406.034/0023-00 Inscrição Estadual 278.205.224.119 Endereço: Rua Passadena 104 - Parque Industrial, Cotia - SP CEP: 06715864.
+          </p>
 
         </footer>
       </main>
